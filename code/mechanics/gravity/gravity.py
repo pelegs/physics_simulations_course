73f6cd35ed17b19086750bbb01655c77a1738279
pelegs <pelegs@gmail.com>
2024-10-30 20:38:38 +0100

    "Refactored" gravity sim to use static size numpy arrays

--- code/mechanics/gravity/gravity.py
+++ code/mechanics/gravity/gravity.py
@@ -4,15 +4,19 @@ from matplotlib.colors import XKCD_COLORS
 from tqdm import tqdm
 
 # Time-related constants, variables and parameters
-num_steps = 5000
+num_steps = 15000
 dt = 0.01
 max_t = num_steps * dt
 time_series = np.arange(0, max_t, dt)
+current_step = 0
 
 # Other constants, variables and parameters
 G = 1.0
 ZERO_VEC = np.zeros(3)
 X_, Y_, Z_ = np.identity(3)
+ZERO_ARRAY = np.zeros((num_steps, 3))
+EMPTY_ARRAY = np.empty((num_steps, 3))
+EMPTY_ARRAY[:] = np.nan
 
 # Colors
 Colors = {name[5:]: HTML_val for name, HTML_val in XKCD_COLORS.items()}
@@ -40,10 +44,11 @@ def sqr_dist(p1, p2):
 
 
 def gravity(p1, p2):
-    r_vec = p2.pos[-1] - p1.pos[-1]
+    r_vec = p2.pos[current_step] - p1.pos[current_step]
     r_dir = normalize(r_vec)
     r2 = np.dot(r_vec, r_vec)
-    return 1.0 * (G * p1.mass * p2.mass / r2) * r_dir
+    F = 1.0 * (G * p1.mass * p2.mass / r2) * r_dir
+    return F
 
 
 # Objects with mass
@@ -57,14 +62,14 @@ class Particle:
         color=Colors["light red"],
         id=-1,
     ):
-        self.pos = np.zeros((1, 3))
+        self.pos = np.copy(EMPTY_ARRAY)
         self.pos[0] = pos_0
 
-        self.vel = np.zeros((1, 3))
+        self.vel = np.copy(ZERO_ARRAY)
         self.vel[0] = vel_0
 
-        self.acc = np.zeros((1, 3))
-        self.force = np.zeros((1, 3))
+        self.acc = np.copy(ZERO_ARRAY)
+        self.force = np.copy(ZERO_ARRAY)
 
         self.mass = mass
         self.mass_inv = 1.0 / mass
@@ -73,28 +78,26 @@ class Particle:
         self.color = color
         self.id = id
 
-    def prepare_next_step(self):
-        self.pos = np.vstack((self.pos, self.pos[-1]))
-        self.vel = np.vstack((self.vel, self.vel[-1]))
-        self.acc = np.vstack((self.acc, np.copy(ZERO_VEC)))
-        self.force = np.vstack((self.force, np.copy(ZERO_VEC)))
-
     def add_force(self, force):
-        self.force[-1] += force
+        self.force[current_step] += force
 
     def verlet_1(self):
-        self.pos[-1] = (
-            self.pos[-2] + self.vel[-2] * dt + 0.5 * self.acc[-2] * dt**2
-        )
+        if current_step >= 1:
+            self.pos[current_step] = (
+                self.pos[current_step - 1]
+                + self.vel[current_step - 1] * dt
+                + 0.5 * self.acc[current_step - 1] * dt**2
+            )
 
     def verlet_2(self):
-        self.acc[-1] = self.force[-1] * self.mass_inv
-        self.vel[-1] = self.vel[-2] + 0.5 * (self.acc[-2] + self.acc[-1]) * dt
-
-
-def prepare_next_step():
-    for particle in particles:
-        particle.prepare_next_step()
+        if current_step >= 1:
+            self.acc[current_step] = self.force[current_step] * self.mass_inv
+            self.vel[current_step] = (
+                self.vel[current_step - 1]
+                + 0.5
+                * (self.acc[current_step - 1] + self.acc[current_step])
+                * dt
+            )
 
 
 def apply_forces():
@@ -116,27 +119,23 @@ def verlet_2():
 
 
 def run():
-    for t in tqdm(time_series):
-        prepare_next_step()
-        apply_forces()
+    global current_step
+    for current_step, time in enumerate(tqdm(time_series)):
         verlet_1()
+        apply_forces()
         verlet_2()
 
 
 def prepare_graphics():
     fig, ax = plt.subplots(1, 1, figsize=(10, 10))
     ax.set_title("Gravity test")
-    ax.set_xlim(-200, 200)
-    ax.set_ylim(-200, 200)
+    ax.set_xlim(-500, 500)
+    ax.set_ylim(-500, 500)
     ax.set_xlabel("x")
     ax.set_ylabel("y")
-    # ax.grid(True)
-    ax.plot(
-        particles[0].pos[:, 0], particles[0].pos[:, 1], c=particles[0].color
-    )
-    ax.plot(
-        particles[1].pos[:, 0], particles[1].pos[:, 1], c=particles[1].color
-    )
+    ax.grid(True)
+    for particle in particles:
+        ax.plot(particle.pos[:, 0], particle.pos[:, 1], c=particle.color)
     plt.show()
 
 
@@ -145,13 +144,22 @@ if __name__ == "__main__":
     planet = Particle(
         id=1,
         pos_0=100 * X_,
-        vel_0=2 * Y_,
-        mass=1.0e3,
+        vel_0=0.7 * np.sqrt(G * star.mass / 100) * Y_,
+        mass=1.0e-4,
         rad=3.0,
         color=Colors["blue"],
     )
     particles = [star, planet]
 
+    # num_particles = 10
+    # particles = [
+    #     Particle(
+    #         id=id,
+    #         pos_0=np.append(np.random.uniform(low=-500, high=500, size=2), 0),
+    #         mass=np.random.uniform(1.0e2, 1.0e6),
+    #     )
+    #     for id in range(num_particles)
+    # ]
+
     run()
     prepare_graphics()
-    # print(particles[1].pos)
