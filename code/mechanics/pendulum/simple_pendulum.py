c33e16bf19ca9d01085c88e514a70215b48732dc
pelegs <pelegs@gmail.com>
2024-11-07 12:34:41 +0100

    Finalized simple pendulum sim

--- code/mechanics/pendulum/simple_pendulum.py
+++ code/mechanics/pendulum/simple_pendulum.py
@@ -4,6 +4,16 @@ from matplotlib.animation import FuncAnimation
 from matplotlib.gridspec import GridSpec
 from tqdm import tqdm
 
+##################################
+#        Helper functions        #
+##################################
+
+
+def rotate(vec, th):
+    c, s = np.cos(th), np.sin(th)
+    return np.dot(np.array([[c, -s], [s, c]]), vec)
+
+
 ############################
 #        Simultaion        #
 ############################
@@ -14,24 +24,41 @@ L = 1.0  # [m]
 
 # Parameters
 t_max = 10.0  # [s]
-dt = 0.05  # [s]
+dt = 0.01  # [s]
 vel_res_sqr = g / L  # [rad/s]
 
 # Variables
 time_series = np.arange(0, t_max, dt)
 num_steps = time_series.shape[0]
 theta = np.zeros(num_steps, dtype=np.float32)
-vel = np.zeros(num_steps, dtype=np.float32)
+omega = np.zeros(num_steps, dtype=np.float32)
 
 # Initial conditions
-theta[0] = np.pi / 4
-vel[0] = 0.0
+theta[0] = np.pi / 2
+omega[0] = 0.0
 
 # Run simulation
 for i, t in enumerate(tqdm(time_series[1:-1]), start=1):
     a_grav = -g / L * np.sin(theta[i - 1])
-    vel[i] = vel[i - 1] + a_grav * dt
-    theta[i] = theta[i - 1] + vel[i] * dt
+    omega[i] = omega[i - 1] + a_grav * dt
+    theta[i] = theta[i - 1] + omega[i] * dt
+
+# Bob position in Cartesian coordinates
+bob_pos = L * np.array([np.sin(theta), -np.cos(theta)]).T
+bob_vel = (
+    rotate(bob_pos.T, np.pi / 2).T
+    * omega.reshape((num_steps, 1))
+    / np.max(omega)
+    * 0.5
+)
+alpha = np.diff(omega)
+bob_acc = (
+    rotate(bob_pos[:-1].T, np.pi / 2).T
+    * alpha.reshape((num_steps - 1, 1))
+    / np.max(alpha)
+    * 10.0
+)
+
 
 ###########################
 #        Animation        #
@@ -59,7 +86,7 @@ ax_phase.set_title("Phase space", fontsize=20)
 ax_phase.set_xlabel(r"$\theta$\ [rad]", fontsize=15)
 ax_phase.set_ylabel(r"$\omega$\ [rad/s]", fontsize=15)
 ax_phase.set_xlim(min(theta) - 0.5, max(theta) + 0.5)
-ax_phase.set_ylim(min(vel) - 0.5, max(vel) + 0.5)
+ax_phase.set_ylim(min(omega) - 0.5, max(omega) + 0.5)
 ax_phase.set_aspect("equal", "box")
 
 # Time plot
@@ -67,42 +94,77 @@ ax_time.set_title("Time plot", fontsize=20)
 ax_time.set_xlabel(r"$t$\ [s]", fontsize=15)
 ax_time.set_ylabel(r"$\theta$\ [rad]", fontsize=15)
 ax_time.set_xlim(0, t_max)
-ax_time.set_ylim((min(theta) - 0.5), max(theta) + 0.5)
+ax_time.set_ylim(-1.2, 1.2)
 
 # Set up everything
-(rod,) = ax_vis.plot(
-    (0, L * np.sin(theta[0])),
-    (0, -L * np.cos(theta[0])),
+rod = ax_vis.plot(
+    (0, bob_pos[0, 0]),
+    (0, bob_pos[0, 1]),
     color="blue",
     solid_capstyle="round",
     lw=3,
-)
-(bob,) = ax_vis.plot(
-    [L * np.sin(theta[0])],
-    [-L * np.cos(theta[0])],
+)[0]
+bob = ax_vis.plot(
+    (bob_pos[0, 0]),
+    (bob_pos[0, 1]),
     "o",
     markersize=20,
     color="red",
-)
-(fix,) = ax_vis.plot([0, 0], [0, 0], "o", markersize=5, color="black")
-(phase_plt,) = ax_phase.plot(theta[0], vel[0], "red")
-(time_plt,) = ax_time.plot(time_series[0], theta[0], "blue")
+)[0]
+fix = ax_vis.plot([0, 0], [0, 0], "o", markersize=5, color="black")[0]
+phase_plt = ax_phase.plot(theta[0], omega[0], "red")[0]
+time_plt_theta = ax_time.plot(time_series[0], theta[0], "blue")[0]
+time_plt_omega = ax_time.plot(time_series[0], omega[0], "green")[0]
+time_plt_omega = ax_time.plot(time_series[0], omega[0], "green")[0]
+time_plt_alpha = ax_time.plot(time_series[0], alpha[0], "purple")[0]
 
 
 # Animation function
 def animate(frame):
-    rod_xs = np.array([0, L * np.sin(theta[frame])])
-    rod_ys = np.array([0, -L * np.cos(theta[frame])])
-    rod.set_data(rod_xs, rod_ys)
+    rod.set_data([0, bob_pos[frame, 0]], [0, bob_pos[frame, 1]])
     bob.set_data(
-        [L * np.sin(theta[frame])],
-        [-L * np.cos(theta[frame])],
+        [bob_pos[frame, 0]],
+        [bob_pos[frame, 1]],
     )
-    phase_plt.set_data(theta[:frame], vel[:frame])
-    time_plt.set_data(time_series[:frame], theta[:frame])
-    return [rod, bob, phase_plt, time_plt]
+    vel_arrow = ax_vis.arrow(
+        bob_pos[frame, 0],
+        bob_pos[frame, 1],
+        bob_vel[frame, 0],
+        bob_vel[frame, 1],
+        color="green",
+        head_width=0.05,
+        head_length=0.1,
+    )
+    acc_arrow = ax_vis.arrow(
+        bob_pos[frame, 0],
+        bob_pos[frame, 1],
+        bob_acc[frame, 0],
+        bob_acc[frame, 1],
+        color="purple",
+        head_width=0.05,
+        head_length=0.1,
+    )
+
+    phase_plt.set_data(theta[:frame], omega[:frame])
+    time_plt_theta.set_data(time_series[:frame], theta[:frame] / np.max(theta))
+    time_plt_omega.set_data(time_series[:frame], omega[:frame] / np.max(omega))
+    time_plt_alpha.set_data(
+        time_series[:frame], alpha[:frame] / np.max(alpha[:-1])
+    )
+    return [
+        rod,
+        bob,
+        vel_arrow,
+        acc_arrow,
+        phase_plt,
+        time_plt_theta,
+        time_plt_omega,
+        time_plt_alpha,
+    ]
 
 
 # Run animation
-anim = FuncAnimation(fig, animate, frames=num_steps, interval=20, blit=True)
+anim = FuncAnimation(
+    fig, animate, frames=num_steps - 1, interval=20, blit=True
+)
 plt.show()
