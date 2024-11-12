8929748f92f7ae356c7bc95f8d096503ff673a25
pelegs <pelegs@gmail.com>
2024-11-12 20:22:53 +0100

    Generated figure for showing different integrators

--- code/mechanics/integrators/integrators.py
+++ code/mechanics/integrators/integrators.py
@@ -14,7 +14,9 @@ k = 2
 omega = np.sqrt(k / m)
 
 # Time related
-dt = 0.2
+dt = 0.1
+dt_half = dt / 2
+dt_sixth = dt / 6
 num_steps = 1000
 L = 1
 times_series = np.linspace(0, num_steps * dt, num_steps)
@@ -28,6 +30,16 @@ v0 = 0.0
 #############################
 
 
+def create_blank_data():
+    xs, vs = np.zeros((2, num_steps))
+    xs[0], vs[0] = x0, v0
+    return xs, vs
+
+
+def derivatives(x, v):
+    return v, -(k / m) * x
+
+
 def exact_solution():
     xs = A * np.cos(omega * times_series + phi)
     vs = -A * omega * np.sin(omega * times_series + phi)
@@ -35,26 +47,58 @@ def exact_solution():
 
 
 def euler_explicit():
-    xs, vs = np.zeros((2, num_steps))
-    xs[0], vs[0] = x0, v0
+    xs, vs = create_blank_data()
     for i, _ in enumerate(
-        tqdm(times_series[1:], desc="Forward Euler"), start=1
+        tqdm(times_series[1:], desc="Explicit Euler"), start=1
     ):
         a = -(k / m) * xs[i - 1]
         vs[i] = vs[i - 1] + a * dt
-        xs[i] = xs[i - 1] + vs[i] * dt
+        xs[i] = xs[i - 1] + vs[i - 1] * dt
 
     return xs, vs
 
 
 def euler_implicit():
-    A = 1 + (dt**2 * k / m)
-    xs, vs = np.zeros((2, num_steps))
+    c1 = 1 + (dt**2 * k / m)
+    c2 = dt * k / m
+    xs, vs = create_blank_data()
+    for i, _ in enumerate(
+        tqdm(times_series[1:], desc="Implicit Euler"), start=1
+    ):
+        xs[i] = (xs[i - 1] + dt * vs[i - 1]) / c1
+        vs[i] = vs[i - 1] - c2 * xs[i]
 
-    for i, _ in enumerate(tqdm(times_series[1:], desc="Forward Euler")):
-        xs[i] = (xs[i - 1] + dt * vs[i - 1]) / A
-        vs[i] = vs[i - 1] + dt * (-k / m * xs[i])
+    return xs, vs
 
+
+def RK2():
+    xs, vs = create_blank_data()
+    for i, _ in enumerate(
+        tqdm(times_series[1:], desc="Forward Euler"), start=1
+    ):
+        k1x, k1v = derivatives(xs[i - 1], vs[i - 1])
+        k2x, k2v = derivatives(xs[i - 1] + dt * k1x, vs[i - 1] + dt * k1v)
+        xs[i] = xs[i - 1] + (dt_half) * (k1x + k2x)
+        vs[i] = vs[i - 1] + (dt_half) * (k1v + k2v)
+    return xs, vs
+
+
+def RK4():
+    xs, vs = create_blank_data()
+    for i, _ in enumerate(
+        tqdm(times_series[1:], desc="Forward Euler"), start=1
+    ):
+        k1x, k1v = derivatives(xs[i - 1], vs[i - 1])
+        k2x, k2v = derivatives(
+            xs[i - 1] + 0.5 * dt * k1x, vs[i - 1] + 0.5 * dt * k1v
+        )
+        k3x, k3v = derivatives(
+            xs[i - 1] + 0.5 * dt * k2x, vs[i - 1] + 0.5 * dt * k2v
+        )
+        k4x, k4v = derivatives(xs[i - 1] + dt * k3x, vs[i - 1] + dt * k3v)
+
+        xs[i] = xs[i - 1] + (dt / 6) * (k1x + 2 * k2x + 2 * k3x + k4x)
+        vs[i] = vs[i - 1] + (dt / 6) * (k1v + 2 * k2v + 2 * k3v + k4v)
     return xs, vs
 
 
@@ -62,9 +106,11 @@ def euler_implicit():
 #        Simulations        #
 #############################
 
-theta_exact, omega_exact = exact_solution()
-theta_euler_explicit, omega_euler_explicit = euler_explicit()
-theta_euler_implicit, omega_euler_implicit = euler_implicit()
+xs_exact, vs_exact = exact_solution()
+xs_euler_explicit, vs_euler_explicit = euler_explicit()
+xs_euler_implicit, vs_euler_implicit = euler_implicit()
+xs_RK2, vs_RK2 = RK2()
+xs_RK4, vs_RK4 = RK4()
 
 ##########################
 #        Graphics        #
@@ -72,20 +118,158 @@ theta_euler_implicit, omega_euler_implicit = euler_implicit()
 
 plt.rcParams.update({"text.usetex": True, "font.family": "Helvetica"})
 
-fig, ax = plt.subplots(figsize=(10, 9), layout="constrained", dpi=200)
-ax.set_title("Simple harmonic oscillator, different integration methods")
-ax.set_xlabel(r"$t$\ [sec]")
-ax.set_ylabel(r"$\theta$\ [rad]")
-ax.set_xlim(0, times_series[-1])
-
-# Plots
-ax.plot(
+# Positions vs time
+fig_xs, ax_xs = plt.subplots(figsize=(10, 9), layout="constrained", dpi=200)
+ax_xs.set_title("Simple harmonic oscillator, different integration methods")
+ax_xs.set_xlabel(r"$t$\ [sec]")
+ax_xs.set_ylabel(r"$x$\ [m]")
+ax_xs.set_xlim(0, times_series[-1])
+ax_xs.plot(
     times_series[:-1],
-    theta_euler_explicit[:-1],
+    xs_euler_explicit[:-1],
     "o",
     markersize=2,
     color="red",
 )
-ax.plot(times_series[:-1], theta_exact[:-1], "black")
+ax_xs.plot(
+    times_series[:-1],
+    xs_euler_implicit[:-1],
+    "o",
+    markersize=2,
+    color="blue",
+)
+ax_xs.plot(
+    times_series[:-1],
+    xs_RK2[:-1],
+    "o",
+    markersize=2,
+    color="green",
+)
+ax_xs.plot(
+    times_series[:-1],
+    xs_RK4[:-1],
+    "o",
+    markersize=2,
+    color="purple",
+)
+ax_xs.plot(times_series[:-1], xs_exact[:-1], "black")
+plt.savefig("pos_vs_time_full.png", bbox_inches="tight")
+plt.close()
 
-plt.show()
+# Velocities vs time
+fig_vs, ax_vs = plt.subplots(figsize=(10, 9), layout="constrained", dpi=200)
+ax_vs.set_title("Simple harmonic oscillator, different integration methods")
+ax_vs.set_xlabel(r"$t$\ [sec]")
+ax_vs.set_ylabel(r"$v$\ [m/s]")
+ax_vs.set_xlim(0, times_series[-1])
+ax_vs.plot(
+    times_series[:-1],
+    vs_euler_explicit[:-1],
+    "o",
+    markersize=2,
+    color="red",
+)
+ax_vs.plot(
+    times_series[:-1],
+    vs_euler_implicit[:-1],
+    "o",
+    markersize=2,
+    color="blue",
+)
+ax_vs.plot(
+    times_series[:-1],
+    vs_RK2[:-1],
+    "o",
+    markersize=2,
+    color="green",
+)
+ax_vs.plot(
+    times_series[:-1],
+    vs_RK4[:-1],
+    "o",
+    markersize=2,
+    color="purple",
+)
+ax_vs.plot(times_series[:-1], vs_exact[:-1], "black")
+plt.savefig("vel_vs_time_full.png", bbox_inches="tight")
+plt.close()
+
+# Positions vs time, partial
+fig_xs, ax_xs = plt.subplots(figsize=(10, 9), layout="constrained", dpi=200)
+ax_xs.set_title("Simple harmonic oscillator, different integration methods")
+ax_xs.set_xlabel(r"$t$\ [sec]")
+ax_xs.set_ylabel(r"$x$\ [m]")
+
+i_max = 100
+ax_xs.set_xlim(0, times_series[i_max])
+ax_xs.plot(
+    times_series[:i_max],
+    xs_euler_explicit[:i_max],
+    "o",
+    markersize=2,
+    color="red",
+)
+ax_xs.plot(
+    times_series[:i_max],
+    xs_euler_implicit[:i_max],
+    "o",
+    markersize=2,
+    color="blue",
+)
+ax_xs.plot(
+    times_series[:i_max],
+    xs_RK2[:i_max],
+    "o",
+    markersize=2,
+    color="green",
+)
+ax_xs.plot(
+    times_series[:i_max],
+    xs_RK4[:i_max],
+    "o",
+    markersize=2,
+    color="purple",
+)
+ax_xs.plot(times_series[:i_max], xs_exact[:i_max], "black")
+plt.savefig("pos_vs_time_partial.png", bbox_inches="tight")
+plt.close()
+
+# Velocity vs time, partial
+fig_vs, ax_vs = plt.subplots(figsize=(10, 9), layout="constrained", dpi=200)
+ax_vs.set_title("Simple harmonic oscillator, different integration methods")
+ax_vs.set_xlabel(r"$t$\ [sec]")
+ax_vs.set_ylabel(r"$v$\ [m]")
+
+i_max = 100
+ax_vs.set_xlim(0, times_series[i_max])
+ax_vs.plot(
+    times_series[:i_max],
+    vs_euler_explicit[:i_max],
+    "o",
+    markersize=2,
+    color="red",
+)
+ax_vs.plot(
+    times_series[:i_max],
+    vs_euler_implicit[:i_max],
+    "o",
+    markersize=2,
+    color="blue",
+)
+ax_vs.plot(
+    times_series[:i_max],
+    vs_RK2[:i_max],
+    "o",
+    markersize=2,
+    color="green",
+)
+ax_vs.plot(
+    times_series[:i_max],
+    vs_RK4[:i_max],
+    "o",
+    markersize=2,
+    color="purple",
+)
+ax_vs.plot(times_series[:i_max], vs_exact[:i_max], "black")
+plt.savefig("vel_vs_time_partial.png", bbox_inches="tight")
+plt.close()
